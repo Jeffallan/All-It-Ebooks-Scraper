cf78d4e7416abc1ac121454bd24ffa21e11e069c
Jeff <dshostakovich30@gmail.com>
2017-12-02 14:55:19 -0600

    first draft

--- ebook/spiders/crawl.py
+++ ebook/spiders/crawl.py
@@ -9,33 +9,35 @@ import socket
 
 class CrawlSpider(SitemapSpider):
     name = 'crawl'
-    sitemap_urls = []
+    sitemap_urls = ['http://www.allitebooks.com/sitemap_index.xml']
 
     def parse(self, response):
 
         l = ItemLoader(item=EbookItem(), response=response)
 
-        #Primary Fields
+        # Primary Fields
         l.add_xpath('Title', '//header/h1/text()')
         l.add_xpath('Subtitle', '//header/h4/text()')
         l.add_xpath('Image', '//img[contains(@class,"attachment-post-thumbnail")]/@src')
         # begin rework table info see nested item loaders
-        l.add_xpath('Author', '')
-        l.add_xpath('ISBIN', '')
-        l.add_xpath('Year', '')
-        l.add_xpath('Pages', '')
-        l.add_xpath('Language', '')
-        l.add_xpath('File_Size', '')
-        l.add_xpath('File_Format', '')
-        l.add_xpath('Category', '')
-        #end rework table info
+        l.add_xpath('Author', '//div[contains(@class, "book-detail")]//dd[1]/a/text()')
+        l.add_xpath('ISBIN', '//div[contains(@class, "book-detail")]//dd[2]/text()')
+        l.add_xpath('Year', '//div[contains(@class, "book-detail")]//dd[3]/text()')
+        l.add_xpath('Pages', '//div[contains(@class, "book-detail")]//dd[4]/text()')
+        l.add_xpath('Language', '//div[contains(@class, "book-detail")]//dd[5]/text()')
+        l.add_xpath('File_Size', '//div[contains(@class, "book-detail")]//dd[6]/text()')
+        l.add_xpath('File_Format', '//div[contains(@class, "book-detail")]//dd[7]/text()')
+        l.add_xpath('Category', '//div[contains(@class, "book-detail")]//dd[8]/text()')
+        # end rework table info
 
-        #MapCompose for Description? lambda s: re.sub(r'[\n\b\f\r\t\v\x00]', ' ', s)
-        l.add_xpath('Description', '//div[contains(@class,"entry-content")])
-        #MapCompose replace ' ' with %20
-        l.add_xpath('Download_Link', '//a[contains(@href,"file")]/@href')
+        # MapCompose for Description? lambda s: re.sub(r'[\n\b\f\r\t\v\x00]', ' ', s)
+        l.add_xpath('Description', '//div[contains(@class,"entry-content")]',
+                    MapCompose(lambda s: re.sub(r'[\n\b\f\r\t\v\x00]', '', s)))
+        # MapCompose replace ' ' with %20
+        l.add_xpath('Download_Link', '//a[contains(@href,"file")]/@href',
+                    MapCompose(lambda i: i.replace(' ', '%20')))
 
-        #Housekeeping Fields
+        # Housekeeping Fields
         l.add_value('url', response.url)
         l.add_value('project', self.settings.get('BOT_NAME'))
         l.add_value('spider', self.name)
