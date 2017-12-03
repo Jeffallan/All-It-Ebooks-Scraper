4df088bb15e6e4ac699ef2c7e33bc693ad11db27
Jeff <dshostakovich30@gmail.com>
2017-12-03 14:33:08 -0600

    added unicode.strip to remove leading and trailing whitespace.

--- ebook/spiders/crawl.py
+++ ebook/spiders/crawl.py
@@ -24,21 +24,26 @@ class CrawlSpider(SitemapSpider):
         l = ItemLoader(item=EbookItem(), response=response)
 
         # Primary Fields
-        l.add_xpath('Title', '//header/h1/text()')
-        l.add_xpath('Subtitle', '//header/h4/text()')
-        l.add_xpath('Image', '//img[contains(@class,"attachment-post-thumbnail")]/@src')
-        # begin rework table info see nested item loaders
-        l.add_xpath('Author', '//div[contains(@class, "book-detail")]//dd[1]/a/text()')
-        l.add_xpath('ISBIN', '//div[contains(@class, "book-detail")]//dd[2]/text()')
-        l.add_xpath('Year', '//div[contains(@class, "book-detail")]//dd[3]/text()')
-        l.add_xpath('Pages', '//div[contains(@class, "book-detail")]//dd[4]/text()')
-        l.add_xpath('Language', '//div[contains(@class, "book-detail")]//dd[5]/text()')
-        l.add_xpath('File_Size', '//div[contains(@class, "book-detail")]//dd[6]/text()')
-        l.add_xpath('File_Format', '//div[contains(@class, "book-detail")]//dd[7]/text()')
-        l.add_xpath('Category', '//div[contains(@class, "book-detail")]//dd[8]//a/text()')
-        # end rework table info
-
-        # MapCompose for Description? lambda s.replace \n \b \f \r \t \v \x00]', '')
+        l.add_xpath('Title', '//header/h1/text()', MapCompose(unicode.strip))
+        l.add_xpath('Subtitle', '//header/h4/text()', MapCompose(unicode.strip))
+        l.add_xpath('Image', '//img[contains(@class,"attachment-post-thumbnail")]/@src',
+                    MapCompose(unicode.strip))
+        l.add_xpath('Author', '//div[contains(@class, "book-detail")]//dd[1]/a/text()',
+                    MapCompose(unicode.strip))
+        l.add_xpath('ISBIN', '//div[contains(@class, "book-detail")]//dd[2]/text()',
+                    MapCompose(unicode.strip))
+        l.add_xpath('Year', '//div[contains(@class, "book-detail")]//dd[3]/text()',
+                    MapCompose(unicode.strip))
+        l.add_xpath('Pages', '//div[contains(@class, "book-detail")]//dd[4]/text()',
+                    MapCompose(unicode.strip))
+        l.add_xpath('Language', '//div[contains(@class, "book-detail")]//dd[5]/text()',
+                    MapCompose(unicode.strip))
+        l.add_xpath('File_Size', '//div[contains(@class, "book-detail")]//dd[6]/text()',
+                    MapCompose(unicode.strip))
+        l.add_xpath('File_Format', '//div[contains(@class, "book-detail")]//dd[7]/text()',
+                    MapCompose(unicode.strip))
+        l.add_xpath('Category', '//div[contains(@class, "book-detail")]//dd[8]//a/text()',
+                    MapCompose(unicode.strip))
         l.add_xpath('Description', '//div[contains(@class,"entry-content")]',
                     MapCompose(lambda s: s.replace('\n', ''),
                                lambda s: s.replace('\b', ''),
@@ -46,16 +51,16 @@ class CrawlSpider(SitemapSpider):
                                lambda s: s.replace('\r', ''),
                                lambda s: s.replace('\t', ''),
                                lambda s: s.replace('\v', ''),
-                               lambda s: s.replace('\x00', ''),))
-        # MapCompose replace ' ' with %20
+                               lambda s: s.replace('\x00', ''),
+                               unicode.strip))
         l.add_xpath('Download_Link', '//a[contains(@href,"file")]/@href',
-                    MapCompose(lambda s: s.replace(' ', '%20')))
+                    MapCompose(lambda s: s.replace(' ', '%20'), unicode.strip))
 
         # Housekeeping Fields
         l.add_value('url', response.url)
         l.add_value('project', self.settings.get('BOT_NAME'))
         l.add_value('spider', self.name)
         l.add_value('server', socket.gethostname())
-        #l.add_value('date', date.today())
+        # l.add_value('date', date.today())
 
         return l.load_item()
